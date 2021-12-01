7edba0f8d185d671e03a25b587659dc579cb0ba5
jonnyspy <36827944+jonnyspy@users.noreply.github.com>
2021-12-01 10:50:45 -0700

    Rotate 3

--- Main.js
+++ Main.js
@@ -964,8 +964,8 @@ function drawPlayer() {
 	if(swordDelay < 9.5) {
 		
 		pushMatrix();
-		translate(playerX,playerY);
-		rotate(swordDelay);
+		translate(playerX+blockSize\2,playerY+blockSize\2);
+		rotate(swordDelay\2);
 		
 		image(sword,0,0,32,32);
 			
